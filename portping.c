8abf76292184c5569eaa588b7ddbf6d460f03a8e
chillymasterio <chillymasterio@gmail.com>
2026-05-16 23:40:18 +0300

    Add --tls flag to verify TLS handshake after TCP connect

--- portping.c
+++ portping.c
@@ -562,6 +562,7 @@ static int g_grab_banner = 0;
 static int g_banner_timeout_ms = 2000;
 static int g_parallel = 1;
 static int g_udp_mode = 0;
+static int g_tls_check = 0;
 
 static void print_timestamp(void) {
     time_t now = time(NULL);

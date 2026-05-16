2dea68de41034308787c307ac81cdcd4f6554cf2
chillymasterio <chillymasterio@gmail.com>
2026-05-16 21:51:11 +0300

    Add --dns-resolve to display resolved IP address in output

--- portping.c
+++ portping.c
@@ -274,6 +274,7 @@ static int g_tap = 0;
 static int g_grace_probes = 0;
 static int g_http_check = 0;
 static int g_expect_http_code = 200;
+static int g_show_resolve = 0;
 
 static int bind_source(SOCKET s, int family) {
     if (!g_source_addr && !g_source_port) return 0;
